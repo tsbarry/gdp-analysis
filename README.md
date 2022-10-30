ce55ba4b1ba876b6f26b1c76918b320adb6fc29e
tsbarry <thiernosbarry2012@gmail.com>
2022-10-30 12:09:04 -0400

    file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# gdp-analysis 
+# gdp-analysis
+
+[quick description of your code]
+
+## Results
+
+[How much did New York drop in GDP from 2019 to 2020? Informally, what sorts of economic factors influenced this change? Provide evidence if relevant.]
+New York gdp from 2019 to 2020 dropped by -73216.0
--- analysis.py
+++ analysis.py
@@ -1,15 +1,26 @@
 import csv
-from re import M
 
 def get_highest_gdp(data, year):
-    Maximum = float(row[0][year])
+    high = float(data[0][year])
     for row in data:
-            val = float(row[year])
-            if val > maximum:
-             maximum = val
-    return maximum
+            value = float(row['2020'])
+            if value > high:
+             high = value
+    return high
+
 def get_lowest_gdp(data, year):
-    pass
+    low = float(data[0][year])
+    for row in data:
+            value = float(row['2020'])
+            if value < low:
+             low = value
+    return low
+
+def get_state_gdp(data, state, year):
+ for row in data:
+    if row['GeoName'] == state:
+        return row[year]
+
 
 # save each row into a list
 list_data = []
@@ -22,6 +33,19 @@ with open("state_gdp_analysis.csv", "r") as infile:
         
 
 # get highest gdp for 2020 using "get_highest_gdp(list_data, '2020')"
-
+print(get_highest_gdp(list_data, '2020'))
 
 # get lowest gdp for 2020 using "get_lowest_gdp(list_data, '2020')"
+print(get_lowest_gdp(list_data, '2020'))
+
+#New York GDP for 2020 using "get_state_gdp(list_data,'New York', '2020'"
+print (get_state_gdp(list_data,'New York', '2020'))
+
+#New York GDP for 2019 using "get_state_gdp(list_data,'New York', '2019'"
+print (get_state_gdp(list_data,'New York', '2019'))
+
+#How much New York droped in GDP from 2019 to 2020
+prev = float(get_state_gdp(list_data, "New York", "2019"))
+new = float(get_state_gdp(list_data, "New York", "2020"))
+
+print (new - prev)
